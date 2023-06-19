c09f97da38b8d1cd5b415b1de0c990e9ac3fff94
IMAGE BHATTARAI <lukasbhata@gmail.com>
2023-06-19 21:14:46 +0545

    dajubunu

---
+++ chor.py
@@ -0,0 +1,30 @@
+b = 44
+if(b>25):
+  print('THE VALUE of A is greater than 25')
+  
+if(b<25):
+  print('THE VALUE OF B IS SMALLER TAHN 25')
+
+if(b>33):
+   print('THE VALUE OF B IS GREATER TO 33')
+else:
+   print('THE VALUE OF B IS GREATER TO 33')
+age = int(input('ENTER YOUR AGE: '))
+if age>18:
+  print('YES You can give vote to the nepali politician')
+else:
+   print('NO')
+favnum = int(input('ENTER YOUR NUM: '))
+if favnum<19:
+  print('YES')
+else:
+  print('NO')
+umer = input(int('ENTER your umer '))
+if(umer>99 and umer<14):
+   print('YOU CAN LOVE ME')
+ages = 23
+if(ages>66 and ages<77):
+   print('WE CAN BE COLLIDE')
+bye = input(int('ENTER YOUR age '))
+if(bye>66 or bye<77):
+  print('YOU CAN WORK WITH ME')
